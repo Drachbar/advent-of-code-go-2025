3cd889d2a00c1777a8816e41f951115619a2a21a
Mattias Andersson <drachbar@gmail.com>
2025-12-03 23:28:19 +0100

    Add solution for Day 03 Part 1 with implementation, tests, and example input

--- internal/day03/solve.go
+++ internal/day03/solve.go
@@ -1,7 +1,37 @@
 package day03
 
+import (
+	"strconv"
+	"strings"
+)
+
 func SolvePart1(data string) int {
-	return 0
+	result := 0
+
+	for _, line := range strings.Split(data, "\n") {
+		chars := []rune(line)
+		firstValAsInt, _ := strconv.Atoi(string(chars[0]))
+		lastValAsInt, _ := strconv.Atoi(string(chars[len(chars)-1]))
+		firstValIndex := 0
+
+		for i := 0; i < len(chars)-1; i++ {
+			val, _ := strconv.Atoi(string(chars[i]))
+			if val > firstValAsInt {
+				firstValAsInt = val
+				firstValIndex = i
+			}
+		}
+
+		for i := firstValIndex + 1; i < len(chars); i++ {
+			val, _ := strconv.Atoi(string(chars[i]))
+			if val > lastValAsInt {
+				lastValAsInt = val
+			}
+		}
+		result += firstValAsInt*10 + lastValAsInt
+	}
+
+	return result
 }
 
 func SolvePart2(data string) int {
--- internal/day03/solve_test.go
+++ internal/day03/solve_test.go
@@ -1,15 +1,52 @@
 package day03
 
 import (
+	"os"
 	"testing"
 )
 
 // PART 1
 
-func TestSolvePart1(t *testing.T) {
-	input := ""
-	want := 0
+func TestSolvePart1Input12Eq12(t *testing.T) {
+	input := "12"
+	want := 12
 	got := SolvePart1(input)
+	if got != want {
+		t.Fatalf("SolvePart1() = %d, want %d", got, want)
+	}
+}
+
+func TestSolvePart1Input123Eq23(t *testing.T) {
+	input := "123"
+	want := 23
+	got := SolvePart1(input)
+	if got != want {
+		t.Fatalf("SolvePart1() = %d, want %d", got, want)
+	}
+}
+
+func TestSolvePart1Input321Eq32(t *testing.T) {
+	input := "321"
+	want := 32
+	got := SolvePart1(input)
+	if got != want {
+		t.Fatalf("SolvePart1() = %d, want %d", got, want)
+	}
+}
+
+func TestSolvePart1Input321n12Eq44(t *testing.T) {
+	input := "321\n12"
+	want := 44
+	got := SolvePart1(input)
+	if got != want {
+		t.Fatalf("SolvePart1() = %d, want %d", got, want)
+	}
+}
+
+func TestSolvePart1ExampleShouldReturn357(t *testing.T) {
+	data, _ := os.ReadFile("example.txt")
+	want := 357
+	got := SolvePart1(string(data))
 	if got != want {
 		t.Fatalf("SolvePart1() = %d, want %d", got, want)
 	}
